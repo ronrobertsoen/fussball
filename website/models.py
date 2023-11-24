a22b2a9bf3fa4ba9ab583cf6889d3ad10376a5a3
ronrobertsoen <robertospelt1@hotmail.com>
2023-11-24 09:02:52 +0100

    login/sign-up sql

---
+++ website/models.py
@@ -0,0 +1,19 @@
+##DATABASE Model #1.20.40
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note') #addiert user.id zu den notes
+
