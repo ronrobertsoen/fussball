e344812145b7114ce292cf397334202a5fdb32a0
ronrobertsoen <robertospelt1@hotmail.com>
2024-01-12 13:54:26 +0100

    comment

--- website/static/index.js
+++ website/static/index.js
@@ -1,10 +1,10 @@
-//Javascript backend code to delete the Event
-
-function deleteEvent(eventId) {
+//Eine Funktion, die aufgerufen wird, um ein Event zu löschen
+function deleteEvent(eventId) { 
+    // Ein 'fetch'-Aufruf, der eine Anfrage an den Server sendet
     fetch('/delete-event',{
-        method: 'POST',
-        body: JSON.stringify({ eventId: eventId})
-    }). then((_res) => {
-        window.location.href = "/"; //redirect
+        method: 'POST', // Die HTTP-Methode, hier POST, um Daten an den Server zu senden
+        body: JSON.stringify({ eventId: eventId}) // Der Körper der Anfrage, hier die Event-ID im JSON-Format
+    }). then((_res) => { // Nachdem die Anfrage verarbeitet wurde, wird diese Funktion ausgeführt
+        window.location.href = "/"; // Leitet den Benutzer zur Hauptseite um
     });
 }
